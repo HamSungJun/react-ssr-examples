7d82ed80ba2c4495ddfb3a46916574ba3525f4a8
함성준 <hamsungjun@hamseongjun-ui-MacBookAir.local>
2022-09-28 17:35:28 +0900

    ADD: Base code for stream version

--- src/Server.tsx
+++ src/Server.tsx
@@ -1,38 +1,79 @@
 import App from "./App";
 import { HelmetProvider, HelmetServerState } from "react-helmet-async";
 import { StaticRouter } from "react-router-dom/server";
-import { renderToString } from "react-dom/server";
+import { PipeableStream, renderToPipeableStream } from "react-dom/server";
+import { Writable } from "node:stream";
 import { parse } from "node-html-parser";
 
 const helmetContext = {} as { helmet: HelmetServerState };
 
 const ServerApp = ({ url }: { url: string }) => {
   return (
-    <HelmetProvider context={helmetContext}>
-      <StaticRouter location={url}>
-        <App />
-      </StaticRouter>
-    </HelmetProvider>
+    <html lang="ko">
+      <head></head>
+      <body>
+        <div id="root">
+          <HelmetProvider context={helmetContext}>
+            <StaticRouter location={url}>
+              <App />
+            </StaticRouter>
+          </HelmetProvider>
+        </div>
+      </body>
+    </html>
   );
 };
 
-export function renderFromServer(template: string, url: string) {
-  const reactAppExecutedHtml = renderToString(<ServerApp url={url} />);
-  const reactRenderedHtml = parse(
-    template.replace("<!--SSR-OUTLET-->", reactAppExecutedHtml)
-  );
+const getPipeableStream = (url: string): Promise<PipeableStream> => {
+  return new Promise((resolve, reject) => {
+    const stream = renderToPipeableStream(<ServerApp url={url} />, {
+      onAllReady() {
+        resolve(stream);
+      },
+      onShellError(error) {
+        reject(error);
+      },
+    });
+  });
+};
 
-  const head = reactRenderedHtml.querySelector("head");
-  const { helmet } = helmetContext;
+export function renderFromServer(url: string) {
+  return new Promise(async (resolve, reject) => {
+    const allResolvedStream = await getPipeableStream(url);
+    const streamBuffer: Uint8Array[] = [];
+    const writer = new Writable({
+      write(chunk, _, callback) {
+        streamBuffer.push(chunk);
+        callback();
+      },
+    });
+    writer.on("finish", () => {
+      const streamToHtmlStr = streamBuffer.toString().trim();
 
-  if (head) {
-    head.innerHTML = `
-        ${head.innerHTML}
-        ${helmet.title.toString()}
-        ${helmet.link.toString()}
-        ${helmet.meta.toString()}
-    `;
-  }
+      const document = parse(streamToHtmlStr);
+      const documentHead = document.querySelector("head");
+      const documentBody = document.querySelector("body");
 
-  return reactRenderedHtml.toString();
+      const { helmet } = helmetContext;
+
+      if (documentHead) {
+        documentHead.innerHTML = `
+          ${documentHead.innerHTML}
+          ${helmet.title.toString()}
+          ${helmet.meta.toString()}
+          ${helmet.link.toString()}
+          ${helmet.priority.toString()}
+        `.trim();
+      }
+
+      if (documentBody) {
+        documentBody.innerHTML = `
+          
+        `;
+      }
+
+      resolve(document.toString());
+    });
+    allResolvedStream.pipe(writer);
+  });
 }
